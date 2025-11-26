65fc1daa865750b9fcb14bb3969ec28cefdb28ab
Oleksandr Fenenko <fenenko@gmail.com>
2025-11-26 20:38:39 +0200

    Створити чисту збірку bergamot і marian статичну

--- scripts/simple-build-translator.sh
+++ scripts/simple-build-translator.sh
@@ -6,29 +6,32 @@ git clone https://github.com/mozilla/translations/
 cd translations
 git checkout 20d1074529279afabbe546efe013f8e1fe80ee24
 cd inference
-mkdir build
-cd build
-cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ../
-cd ..
-rm -rf build
-cd ..
-git apply  ../../../scripts/translations.patch
-cd inference 
-mkdir build
-cd build
-cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5  \
--DUSE_RUY=ON \
--DBUILD_SHARED_LIBS=on \
--DCMAKE_PREFIX_PATH=$(readlink -f ../../../../) \
--DCMAKE_INSTALL_PREFIX=$(readlink -f ../../../../) \
--DUSE_STATIC_LIBS=off \
--DCOMPILE_CPU=on \
--DCOMPILE_CUDA=off \
-../
+git submodule update --init --recursive
+#mkdir build
+#cd build
+#cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ../
+#cd ..
+#rm -rf build
+#cd ..
+#git apply  ../../../scripts/translations.patch
+#cd marian-fork
+#git apply ../../../../../scripts/translations.patch
+#cd inference 
+#mkdir build
+#cd build
+#cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5  \
+#-DUSE_RUY=ON \
+#-DCMAKE_PREFIX_PATH=$(readlink -f ../../../../) \
+#-DCMAKE_INSTALL_PREFIX=$(readlink -f ../../../../) \
+#-DUSE_STATIC_LIBS=on \
+#-DCOMPILE_CPU=on \
+#-DCOMPILE_CUDA=off \
+#../
 
 #	-DUSE_RUY=on \
 #-DCBLAS_LIBRARIES=$(readlink -f ../../../../lib64/libcblas.a) \
 #-DTcmalloc_LIBRARIES=$(readlink -f ../../../../lib/libtcmalloc_minimal.so) \
 #-DTcmalloc_INCLUDE_DIR=$(readlink -f ../../../../include) \
 
+#-DBUILD_SHARED_LIBS=on \
 
