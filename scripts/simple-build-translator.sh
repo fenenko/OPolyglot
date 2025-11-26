724ad503122970b415ca936fcdfb55869eacf250
Oleksandr Fenenko <fenenko@gmail.com>
2025-11-27 01:23:37 +0200

    Налаштування збірки libbergamot

--- scripts/simple-build-translator.sh
+++ scripts/simple-build-translator.sh
@@ -7,6 +7,7 @@ cd translations
 git checkout 20d1074529279afabbe546efe013f8e1fe80ee24
 cd inference
 git submodule update --init --recursive
+cp ../3rd_party/marian-dev/Doxyfile.in marian-fork/
 #mkdir build
 #cd build
 #cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ../
@@ -19,19 +20,19 @@ git submodule update --init --recursive
 #cd inference 
 #mkdir build
 #cd build
-#cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5  \
-#-DUSE_RUY=ON \
-#-DCMAKE_PREFIX_PATH=$(readlink -f ../../../../) \
-#-DCMAKE_INSTALL_PREFIX=$(readlink -f ../../../../) \
-#-DUSE_STATIC_LIBS=on \
-#-DCOMPILE_CPU=on \
-#-DCOMPILE_CUDA=off \
-#../
-
+cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5  \
+-DUSE_RUY=ON \
+-DCMAKE_PREFIX_PATH=$(readlink -f ../../../../) \
+-DCMAKE_INSTALL_PREFIX=$(readlink -f ../../../../) \
+-DUSE_STATIC_LIBS=off \
+-DBUILD_SHARED_LIBS=on \
+-DCMAKE_BUILD_TYPE=Release \
+-DCOMPILE_CPU=on \
+-DCOMPILE_CUDA=off \
+.. 
 #	-DUSE_RUY=on \
 #-DCBLAS_LIBRARIES=$(readlink -f ../../../../lib64/libcblas.a) \
 #-DTcmalloc_LIBRARIES=$(readlink -f ../../../../lib/libtcmalloc_minimal.so) \
 #-DTcmalloc_INCLUDE_DIR=$(readlink -f ../../../../include) \
 
-#-DBUILD_SHARED_LIBS=on \
 
