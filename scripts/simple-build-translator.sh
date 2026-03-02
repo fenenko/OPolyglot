53ec3372561cbb82ffaaef067862164392573cf8
Oleksandr Fenenko <fenenko@gmail.com>
2026-03-02 22:54:41 +0200

    Написана проста збірка для linux

--- scripts/simple-build-translator.sh
+++ scripts/simple-build-translator.sh
@@ -5,28 +5,11 @@ mkdir -p ../bin
 cd ../build/src
 git clone https://github.com/mozilla/translations/
 cd translations
-cd inference
 git submodule update --init --recursive
-git checkout -b opolyglot 20d1074529279afabbe546efe013f8e1fe80ee24
-cp ../3rd_party/marian-dev/Doxyfile.in marian-fork/
-git apply ../../../../scripts/inference.patch
-#git add CMakeLists.txt src/translator/CMakeLists.txt marian-fork/src/CMakeLists.txt marian-fork/Doxyfile.in
-#git commit -m "configure for build shared library"
+git apply ../../../scripts/translations.patch
 mkdir build
 cd build
-cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5  \
-	-DUSE_STATIC_LIBS=off \
-	-DBUILD_SHARED_LIBS=on \
-	-DCMAKE_BUILD_TYPE=Release \
-	-DCOMPILE_CPU=on \
-	-DCOMPILE_CUDA=off \
-	-DSSPLIT_USE_INTERNAL_PCRE2=on \ 
-	..
-
+cmake ..
 make
-cp libmarian.so ../../../../../bin/
-cp src/translator/libbergamot-translator-source.so ../../../../../bin/
-
-
-#	-DCMAKE_PREFIX_PATH=$(readlink -f ../../../../) \
-#	-DCMAKE_INSTALL_PREFIX=$(readlink -f ../../../../) \
+cp libmarian.so ../../../../bin/
+cp inference/src/translator/libbergamot-translator-source.so ../../../../bin/
