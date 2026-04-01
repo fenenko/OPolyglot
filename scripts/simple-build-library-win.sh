b03ec755b3e48218dfa038653a0f2748f5be0ca4
Oleksandr Fenenko <fenenko@gmail.com>
2026-04-01 17:22:29 +0300

    Збірка всіх бібліотек для MINGW в одному місці

--- scripts/simple-build-library-win.sh
+++ scripts/simple-build-library-win.sh
@@ -8,6 +8,8 @@ if [ ! -f "../mingw64/include/zlib.h" ]; then
 	wget https://www.zlib.net/zlib-1.3.2.tar.gz
 	tar -xvf zlib-1.3.2.tar.gz
 	cd ./zlib-1.3.2
+	cp ../../../patch/zlib.patch
+	patch -p0 < zlib.patch
 	mkdir build-win
 	cd build-win
 	cmake -DCMAKE_TOOLCHAIN_FILE=../../../../scripts/mingw-toolchain.cmake \
@@ -15,6 +17,17 @@ if [ ! -f "../mingw64/include/zlib.h" ]; then
 									-DZLIB_BUILD_STATIC=OFF \
 									../
 	make
+	make install
+	cd ../../
+fi
+if [ ! -f "../mingw64/include/pcre2.h" ]; then
+	git clone https://github.com/PCRE2Project/pcre2
+	cd pcre2
+	./autogen.sh 
+	mkdir build-win
+	cd build-win
+	../configure --prefix=$(readlink -f ../../../mingw64) --host=x86_64-w64-mingw32 --build=x86_64-linux
+	make 
 	make install
 	cd ../../
 fi
@@ -46,7 +59,7 @@ if [ ! -f "../mingw64/include/wx-3.2/wx/wx.h" ]; then
 	cd wxWidgets-3.2.10
 	mkdir build-win
 	cd build-win
-	../configure --host=x86_64-w64-mingw32 --build=x86_64-linux-gnu --prefix=$(readlink -f ../../../mingw64) --enable-unicode --enable-shared --with-msw --disable-debug --enable-monolithic
+	cmake -DCMAKE_TOOLCHAIN_FILE=../../../../scripts/mingw-toolchain.cmake -DCMAKE_INSTALL_PREFIX=../../../mingw64 -DwxUSE_LIBPNG=sys -DwxUSE_ZLIB=sys -DwxUSE_LIBTIFF=sys -DwxUSE_LIBJPEG=OFF -DwxUSE_WEBVIEW=OFF ../
 	make
 	make install
 	cd ../../
@@ -83,14 +96,44 @@ if [ ! -f "../mingw64/include/leptonica/allheaders.h" ]; then
 	make install
 	cd ../../
 fi
-git clone https://github.com/tesseract-ocr/tesseract
-cd tesseract
-git checkout 5.5.2
-./autogen.sh
-mkdir build
-cd build
-LEPTONICA_CFLAGS="-I$(readlink -f ../../../mingw64/include/leptonica)" LEPTONICA_LIBS="-L$(readlink -f ../../../mingw64/lib) -lleptonica.dll" ../configure --disable-debug --host=x86_64-w64-mingw32 --build=x86_64-linux-gnu --prefix=$(readlink -f ../../../mingw64)
-make 
-make install
-
-
+if [ ! -f "../mingw64/include/tesseract/baseapi.h" ]; then
+	git clone https://github.com/tesseract-ocr/tesseract
+	cd tesseract
+	git checkout 5.5.2
+	./autogen.sh
+	mkdir build
+	cd build
+	LEPTONICA_CFLAGS="-I$(readlink -f ../../../mingw64/include/leptonica)" LEPTONICA_LIBS="-L$(readlink -f ../../../mingw64/lib) -lleptonica.dll" ../configure --disable-debug --host=x86_64-w64-mingw32 --build=x86_64-linux-gnu --prefix=$(readlink -f ../../../mingw64)
+	make 
+	make install
+	cd ../../
+fi
+if [ ! -f "../mingw64/include/openblas/cblas.h" ]; then
+	git clone https://github.com/OpenMathLib/OpenBLAS
+	cd OpenBLAS
+	git checkout v0.3.29
+	mkdir build
+	cd build
+	cmake  -DCMAKE_TOOLCHAIN_FILE=../../../../scripts/mingw-toolchain.cmake -DCMAKE_BUILD_TYPE=Release  -DDYNAMIC_ARCH=0 -DBINARY=64 -DNO_AVX=1 -DNO_AVX2=1 -DUSE_THREAD=0 -DNO_AFFINITY=1 -DTARGET=CORE2 -DBUILD_SHARED_LIBS=ON -DNOFORTRAN=1 -DCMAKE_INSTALL_PREFIX=$(readlink -f ../../../mingw64) ../
+	make 
+	make install
+	cd ../../
+fi
+if [ ! -f "../mingw64/bin/libmarian.dll" ]; then
+	git clone https://github.com/mozilla/translations/
+	cd translations
+	git submodule update --init --recursive
+	git apply ../../../patch/translations.mingw.patch
+	mkdir build
+	cd build
+	cmake -DCMAKE_TOOLCHAIN_FILE=../../../../scripts/mingw-toolchain.cmake -DCMAKE_BUILD_TYPE=Release ../
+	make
+	cp inference/marian-fork/src/libmarian.dll ../../../../bin
+	cp inference/src/translator/libbergamot-translator-source.dll ../../../../bin
+	cp inference/marian-fork/src/libmarian.dll ../../../mingw64/bin
+	cp inference/src/translator/libbergamot-translator-source.dll ../../../mingw64/bin
+	cp libmarian.dll.a	../../../mingw64/lib
+	cp inference/src/translator/libbergamot-translator-source.dll.a ../../../mingw64/lib
+	cd ..
+	cp -r inference/ ../../mingw64/include
+fi
