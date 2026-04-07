ffa48bceebfa27628130b7f700ef9b9a22fed292
Oleksandr Fenenko <fenenko@gmail.com>
2026-04-07 12:26:29 +0300

    tomcrypt переведено на CMAKE

--- scripts/simple-build-library-win.sh
+++ scripts/simple-build-library-win.sh
@@ -71,8 +71,8 @@ if [ ! -f "../mingw64/include/tommath.h" ]; then
 	git clone https://github.com/libtom/libtommath
 	cd libtommath
 	git checkout v1.3.0
-	mkdir build-win
-	cd build-win
+	mkdir build-mingw64
+	cd build-mingw64
 	cmake -DCMAKE_TOOLCHAIN_FILE=../../../../scripts/mingw-toolchain.cmake -DCMAKE_INSTALL_PREFIX=../../../mingw64 -DBUILD_SHARED_LIBS=ON  ../
 	make
 	make install
@@ -82,10 +82,13 @@ if [ ! -f "../mingw64/include/tomcrypt.h" ]; then
 	git clone https://github.com/libtom/libtomcrypt
 	cd libtomcrypt
 	git checkout v1.18.2
-	git apply ../../../patch/libtomcrypt.mingw.patch
-	make -f makefile.mingw CC=x86_64-w64-mingw32-gcc CFLAGS="-U_FORTIFY_SOURCE -O2 -Wall"
-	make -f makefile.mingw install
-	cd ..
+	#git apply ../../../patch/libtomcrypt.mingw.patch
+	mkdir build-mingw64
+	cd build-mingw64
+	cmake -DCMAKE_TOOLCHAIN_FILE=../../../../scripts/mingw-toolchain.cmake -DCMAKE_INSTALL_PREFIX=../../../mingw64 -DBUILD_SHARED_LIBS=ON  ../
+	#make -f makefile.mingw CC=x86_64-w64-mingw32-gcc CFLAGS="-U_FORTIFY_SOURCE -O2 -Wall"
+	#make -f makefile.mingw install
+	cd ../../
 fi
 if [ ! -f "../mingw64/include/leptonica/allheaders.h" ]; then
 	git clone https://github.com/DanBloomberg/leptonica
