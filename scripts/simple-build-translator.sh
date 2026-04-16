9390b38e38f073a880acae3b7e93a3ba832c6c35
Oleksandr Fenenko <fenenko@gmail.com>
2026-04-16 16:30:38 +0300

    Added the bergamot library build for Linux

---
+++ scripts/simple-build-translator.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+mkdir -p ../build/src
+mkdir -p ../build/linux/bin
+mkdir -p ../build/linux/lib
+mkdir -p ../build/linux/include
+mkdir -p ../bin
+cd ../build/src
+git clone https://github.com/mozilla/translations/
+cd translations
+git submodule update --init --recursive
+git apply ../../../patch/translations.patch
+mkdir build
+cd build
+cp -r ../inference/ ../../../linux/include
+cmake ..
+make
+cp libmarian.so ../../../../bin/
+cp inference/src/translator/libbergamot-translator-source.so ../../../../bin/
+cp libmarian.so ../../../linux/bin/
+cp inference/src/translator/libbergamot-translator-source.so ../../../linux/bin/
+cd ..
