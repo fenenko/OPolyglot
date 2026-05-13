99739a01f807fc614da6584717dae3ad7f819182
Oleksandr Fenenko <fenenko@gmail.com>
2026-05-13 14:59:06 +0300

    Add Leptonica and Tesseract libraries

--- scripts/simple-build-translator.sh
+++ scripts/simple-build-translator.sh
@@ -6,6 +6,34 @@ mkdir -p ../build/linux/lib
 mkdir -p ../build/linux/include
 mkdir -p ../bin
 cd ../build/src
+if [ ! -f "./1.87.0.tar.gz" ]; then
+	wget https://github.com/DanBloomberg/leptonica/archive/refs/tags/1.87.0.tar.gz
+	tar -xvf 1.87.0.tar.gz
+fi
+cd leptonica-1.87.0
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=../../../linux -DBUILD_SHARED_LIBS=ON -DSW_BUILD=OFF ../
+make 
+make install
+cd ../
+rm -rf build
+cd ../
+if [ ! -f "./5.5.2.tar.gz" ]; then
+	wget -nv https://github.com/tesseract-ocr/tesseract/archive/refs/tags/5.5.2.tar.gz
+	tar -xf 5.5.2.tar.gz
+fi
+cd tesseract-5.5.2
+./autogen.sh
+mkdir build-linux
+cd build-linux
+LEPTONICA_CFLAGS="-I$(readlink -f ../../../linux/include/leptonica)" LEPTONICA_LIBS="-L$(readlink -f ../../../linux/lib) -lleptonica" ../configure --disable-debug --build=x86_64-linux-gnu --prefix=$(readlink -f ../../../linux)
+echo "Build tesseract $(date)"
+make 
+make install
+cd ../
+rm -rf build-linux
+cd ..
 git clone https://github.com/mozilla/translations/
 cd translations
 git submodule update --init --recursive
